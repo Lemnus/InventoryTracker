8567f4071aa43d3f4e4e228c1894c403e974a00a
Sirb Sebi <sebisarb@yahoo.com>
2020-07-08 18:55:31 +0300

    Add project files.

---
+++ InventoryTracker/Model/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryTracker.Model
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public int StockCount { get; set; }
+    }
+}
